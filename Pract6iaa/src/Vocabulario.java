0de9c44cbba862d99efc1a5d5e4a07a2da8eeb48
alu0100888102 <alu0100888102@ull.edu.es>
2017-04-20 13:59:46 +0100

    Programa para vocabulario empezado

---
+++ Pract6iaa/src/Vocabulario.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import java.io.*;
+
+public class Vocabulario {
+	Hashtable<String, Integer> palabras;
+	
+	public Vocabulario(){
+		palabras = new Hashtable<String, Integer>();
+	}
+	public Vocabulario(File input){
+		palabras = new Hashtable<String, Integer>();
+		try{
+			FileInputStream istream = new FileInputStream(input);
+			BufferedReader bufferreader = new BufferedReader(new InputStreamReader(istream));
+			String line = null;
+			AtomicBoolean comment = new AtomicBoolean(false);
+			while ((line = bufferreader.readLine()) != null) {
+				if(line.isEmpty())
+					continue;
+				String[] division = line.split("\\s+");
+				for(String text : division){
+					if(palabras.contains(text)){
+						palabras.put(text, palabras.get(text)+1);
+					}
+					else
+						palabras.put(text, 1);
+				}
+			}
+			bufferreader.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("Error en el fichero: no se encuentra " + e);
+			System.exit(1);
+		}
+		catch(IOException e){
+			System.out.println("Error en el fichero: error de entrada/salida " + e);
+			System.exit(1);
+		}
+		catch(IllegalArgumentException e){
+			System.out.println("Linea  Error en el fichero: error de entrada/salida " + e);
+			System.exit(1);
+		}
+	}
+}
