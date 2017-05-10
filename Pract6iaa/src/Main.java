821920acb79c02773fa4ebc188e1891345dd088e
Joaquin Sanchiz <alu0100893755@ull.edu.es>
2017-05-10 18:58:56 +0100

    Update

--- Pract6iaa/src/Main.java
+++ Pract6iaa/src/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
 	
 	public static void main(String[] args){
-		for(int i =1; i<=20; i++){
+		/*for(int i =1; i<=20; i++){
 			String in = new String("corpus/corpus"+i+".txt");
 			String out= new String("aprendizaje/aprendizaje"+i+".txt");
 			File input = new File(in);
@@ -13,7 +13,12 @@ public class Main {
 			Vocabulario vocabulario = new Vocabulario(input);
 			vocabulario.suavizadoLaplaciano(todo, output);
 			System.out.print("====================================================\nCorpus "+i+" Completo\n====================================================\n");
-		}
+		}*/
+		
+		Clasificacion clasificador = new Clasificacion();
+		clasificador.aprender();
+		clasificador.analizar(new File("corpus/corpustodo.txt"), new File("outputs/output.txt"));
+		
 	}
 
 }
