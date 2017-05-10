8923c8a88cf685f82ac0f48bf8414ad6097e4fb3
alu0100888102 <alu0100888102@ull.edu.es>
2017-05-10 18:37:31 +0100

    De momento terminado el clasificacion

--- Pract6iaa/src/Clasificacion.java
+++ Pract6iaa/src/Clasificacion.java
@@ -1,8 +1,5 @@
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
 public class Clasificacion {
@@ -19,15 +16,16 @@ public class Clasificacion {
 		try{
 			for(int i =1; i<=20; i++){
 				//Declaraciones de variables
-				FileInputStream istreamVocabulario = new FileInputStream("aprendizaje/aprendizaje"+i+".txt");
+				FileInputStream istreamVocabulario = new FileInputStream(new File("aprendizaje/aprendizaje"+i+".txt"));
 				BufferedReader bufferedReaderVocabulario = new BufferedReader(new InputStreamReader(istreamVocabulario));
-				String lineVocabulario = null;
-				lineVocabulario =bufferedReaderVocabulario.readLine();
+				String line = null;
+				line =bufferedReaderVocabulario.readLine();
+				line =bufferedReaderVocabulario.readLine();
 				
 				//Coge cada palabra del fichero de aprendizaje
 				Hashtable<String, Double> palabras = new Hashtable<String, Double>();
-				while((lineVocabulario = bufferedReaderVocabulario.readLine()) != null){
-					String[] data = lineVocabulario.split("\\s+");
+				while((line = bufferedReaderVocabulario.readLine()) != null){
+					String[] data = line.split("\\s+");
 					palabras.put(data[1], Double.parseDouble(data[5])); //Cogemos el String de la palabra y su LogProb
 				}
 				vocabularios.add(palabras);
@@ -38,4 +36,68 @@ public class Clasificacion {
 			e.printStackTrace();
 		}
 	}
+	
+	public void analizar(File toAnalize, File outputFile){
+		try{
+			//Limpiamos el fichero de salida
+			PrintWriter writer = new PrintWriter(outputFile);
+			writer.print("");
+			writer.close();
+			//Declaraciones de variables
+			FileInputStream istreamVocabulario = new FileInputStream(toAnalize);
+			BufferedReader bufferedReaderVocabulario = new BufferedReader(new InputStreamReader(istreamVocabulario));
+			String line = null;
+			
+			//Iteración entre las palabras del vocabulario y las del corpus
+			while((line = bufferedReaderVocabulario.readLine()) != null){
+				int perteneceA = 0;
+				double greater = Double.MIN_VALUE;
+				String[] data = line.split("\\s+");
+				
+				for(int i = 0; i< vocabularios.size(); i++){
+					double value = 0;
+					for(String word : data){
+						value += vocabularios.get(i).get(word);
+					}
+					if(value > greater){
+						greater = value;
+						perteneceA = i+1;
+					}
+				}
+				
+				//Escritura de la linea
+				this.writeLineToFile(outputFile, "Frase: " + line + "Pertenece a: "+ perteneceA);
+					
+				//Mensaje para el debuggin
+				System.out.println(line);
+					
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeLineToFile(File out, String line){
+		FileWriter fichero = null;
+		PrintWriter pw = null;
+		try{
+			fichero = new FileWriter(out, true);
+			pw = new PrintWriter(fichero);
+			pw.println(line);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			try{
+				if(null != fichero)
+					fichero.close();
+			}
+			catch (Exception e2){
+				e2.printStackTrace();
+			}
+		}
+	}
 }
+
